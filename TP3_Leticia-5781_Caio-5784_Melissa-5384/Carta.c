8e8040efab1c3e7001d9d612128bbd84beb30119
Melissa Alanis <melissaalanis1246@gmail.com>
2023-11-16 21:00:55 -0300

    Add files via upload

--- TP3_Leticia-5781_Caio-5784_Melissa-5384/Carta.c
+++ TP3_Leticia-5781_Caio-5784_Melissa-5384/Carta.c
@@ -7,16 +7,36 @@ void Inicializa_Carta(Carta* carta, int numero, char cor){
 
 void Printar_Carta(Carta* carta, int numero, char cor){
     if (carta -> cor == 'R'){
+        if((carta -> numero) < 10){
+            printf(cor_vermelha "0%d" resetar_cor  "\n", carta -> numero );
+        }
+        else{
         printf(cor_vermelha "%d" resetar_cor  "\n", carta -> numero );
+        }
     }
     else if (carta -> cor == 'G'){
+        if((carta -> numero) < 10){
+            printf(cor_verde "0%d" resetar_cor  "\n", carta -> numero );
+        }
+        else{
         printf(cor_verde "%d" resetar_cor  "\n" , carta -> numero);
+        }
     }
     else if (carta -> cor == 'B'){
+        if((carta -> numero) < 10){
+            printf(cor_azul "0%d" resetar_cor  "\n", carta -> numero );
+        }
+        else{
         printf(cor_azul "%d" resetar_cor "\n", carta -> numero);
+        }
     }
     else if (carta -> cor == 'Y'){
+        if((carta -> numero) < 10){
+            printf(cor_amarela "0%d" resetar_cor  "\n", carta -> numero );
+        }
+        else{
         printf(cor_amarela "%d" resetar_cor "\n", carta -> numero);
+        }
     }
     else if (carta -> cor == 'P'){
         printf(cor_preta "\x1b[30m" "%d" resetar_cor "\n", carta -> numero);}
